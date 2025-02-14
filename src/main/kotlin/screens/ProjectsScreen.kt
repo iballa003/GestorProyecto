16886c914225b3c028ebca75c7526e869da760c9
iballa003 <iballa.montero@gmail.com>
2025-02-14 09:53:40 +0000

    Creando el ProjectsScreen.kt

--- src/main/kotlin/screens/ProjectsScreen.kt
+++ src/main/kotlin/screens/ProjectsScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -12,12 +14,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import model.Project
 import model.User
+import network.getProjects
 
 class ProyectsScreen(user: User) : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
+        val proyectsList = remember { mutableStateOf(emptyList<Project>()) }
+        getProjects {
+            proyectsList.value = it
+        }
         Column(
             modifier = Modifier.fillMaxSize().padding(top = 28.dp),
             //verticalArrangement = Arrangement.Center,
@@ -30,42 +38,12 @@ class ProyectsScreen(user: User) : Screen {
                 Text(text = "Proyectos", fontWeight = FontWeight.Bold, fontSize = 28.sp)
             }
             LazyColumn(){
-                item {
-                    Text(
-                        text = "Item 1",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
-                item {
-                    Text(
-                        text = "Item 2",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
-                item {
-                    Text(
-                        text = "Item 3",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
-                item {
-                    Text(
-                        text = "Item 4",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
-                item {
-                    Text(
-                        text = "Item 5",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
-                item {
-                    Text(
-                        text = "Item 6",
-                        modifier = Modifier.padding(16.dp),
-                    )
-                }
+//                items() {
+//                    Text(
+//                        text = "Item 1",
+//                        modifier = Modifier.padding(16.dp),
+//                    )
+//                }
             }
         }
     }
